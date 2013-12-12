a6ba4fbb7358c4fdd50b47746bc10d8df89393eb
claudioccm <gigeer@gmail.com>
2013-12-12 19:51:24 -0200

    more updates to teh new pages
    candidates page is just on design mode

--- static/js/ui-scripts.js
+++ static/js/ui-scripts.js
@@ -7,23 +7,22 @@ $('.m-list-company-summary').click(function() {
 
 
 
-//Istopes
-// init Isotope
-var $container = $('.isotopes-container');
-// init
-$container.isotope({
-  // options
-  itemSelector: '.m-candidates-item',
-  layoutMode: 'fitColumns'
 });
 
+// //Istopes
+// // init Isotope
+// var $container = $('.isotopes-container');
+// // init
+// $container.isotope({
+//   // options
+//   itemSelector: '.m-candidates-item',
+//   layoutMode: 'fitColumns'
+// });
 
 
-// filter items on button click
-$('#filters').on( 'click', 'button', function( event ) {
-  var filtr = $(this).attr('data-filter');
-  $container.isotope({ filter: filtr });
-});
 
-
-});
+// // filter items on button click
+// $('#filters').on( 'click', 'button', function( event ) {
+//   var filtr = $(this).attr('data-filter');
+//   $container.isotope({ filter: filtr });
+// });
